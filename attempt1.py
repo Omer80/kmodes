839bed771194f5a1385eec7da34ea9f636d1adc0
Omer80 <omertz@post.bgu.ac.il>
2015-02-03 11:52:05 +0200

    first commit

---
+++ attempt1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  attempt1.py
+#  
+#  Copyright 2015 Ohm <ohm@ohm-S400CA>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import numpy as np
+
+def rolling_window_lastaxis(a, window):
+    """Directly taken from Erik Rigtorp's post to numpy-discussion.
+    <http://www.mail-archive.com/numpy-discussion@scipy.org/msg29450.html>"""
+    if window < 1:
+       raise ValueError, "`window` must be at least 1."
+    if window > a.shape[-1]:
+       raise ValueError, "`window` is too long."
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
+def rolling_window(a, window):
+    if not hasattr(window, '__iter__'):
+        return rolling_window_lastaxis(a, window)
+    for i, win in enumerate(window):
+        if win > 1:
+            a = a.swapaxes(i, -1)
+            a = rolling_window_lastaxis(a, win)
+            a = a.swapaxes(-2, i)
+    return a
+
+
+
+def main():
+	
+	filtsize = (3, 3)
+	a = np.zeros((10,10), dtype=np.float)
+	a[5:7,5] = 1
+	
+	b = rolling_window(a, filtsize)
+	blurred = b.mean(axis=-1).mean(axis=-1)
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
+
