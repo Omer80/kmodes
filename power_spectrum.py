df544b3e046d422075e392f1bfe65de1da39c9d2
Omer80 <omertz@post.bgu.ac.il>
2015-02-15 15:44:12 +0200

    working power_spectrum

--- power_spectrum.py
+++ power_spectrum.py
@@ -28,13 +28,14 @@ import numpy as np
 import pylab as py
 import radialprofile
 
-def main(imagefile = "SH.png"):
+def main(imagefile = "IsoToRaster01_small.jpg"):
 		
-	
 	image = cv2.imread("./data/"+imagefile)
+	im_gray = cv2.imread("./data/"+imagefile, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 	 
 	# Take the fourier transform of the image.
-	F1 = fftpack.fft2(image)
+	F1 = fftpack.fft2(im_bw)
 	 
 	# Now shift the quadrants around so that low spatial frequencies are in
 	# the center of the 2D fourier transformed image.
@@ -44,7 +45,7 @@ def main(imagefile = "SH.png"):
 	psd2D = np.abs( F2 )**2
 	 
 	# Calculate the azimuthally averaged 1D power spectrum
-	psd1D = radialProfile.azimuthalAverage(psd2D)
+	psd1D = radialprofile.azimuthalAverage(psd2D)
 	 
 	# Now plot up both
 	py.figure(1)
@@ -53,11 +54,11 @@ def main(imagefile = "SH.png"):
 	 
 	py.figure(2)
 	py.clf()
-	py.imshow( np.log10( psf2D ))
+	py.imshow( np.log10( psd2D ))
 	 
 	py.figure(3)
 	py.clf()
-	py.semilogy( psf1D )
+	py.semilogy( psd1D )
 	py.xlabel('Spatial Frequency')
 	py.ylabel('Power Spectrum')
 	 
